4d3f0df0d4c352e810fd51840797930f9e585155
HolyCityAudio <faultline1989@yahoo.com>
2016-02-18 22:17:28 -0800

    Build 993.

--- src/com/holycityaudio/SpinCAD/SpinCADFrame.java
+++ src/com/holycityaudio/SpinCAD/SpinCADFrame.java
@@ -94,7 +94,7 @@ public class SpinCADFrame extends JFrame {
 	 * 
 	 */
 
-	int buildNum = 992;
+	int buildNum = 993;
 	// Swing things
 	private JPanel contentPane;
 	//=====================s====================================================================
