91a93168a17ffbf3a6050642412ae55a426b36c2
Gary <Gary@192.168.2.16>
2017-04-29 22:21:36 -0700

    Checking build 1007

--- src/com/holycityaudio/SpinCAD/SpinCADFrame.java
+++ src/com/holycityaudio/SpinCAD/SpinCADFrame.java
@@ -94,7 +94,7 @@ public class SpinCADFrame extends JFrame {
 	 * 
 	 */
 
-	int buildNum = 1006;
+	int buildNum = 1007;
 	// Swing things
 	private JPanel contentPane;
 	//=====================s====================================================================
