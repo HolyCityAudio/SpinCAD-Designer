4161b33a97e33bbe26f5f86404b75a56f4eeed36
Digital-Larry <gary.worsham@gmail.com>
2022-07-07 07:41:02 -0700

    Cleaned up some unused imports.

--- src/com/holycityaudio/SpinCAD/SpinCADPanel.java
+++ src/com/holycityaudio/SpinCAD/SpinCADPanel.java
@@ -37,7 +37,6 @@ import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
