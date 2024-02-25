a26df7bb82283e60542da10f3d4078c3f40ffb7e
Gary Worsham <gary.worsham@gmail.com>
2024-02-25 10:40:43 -0800

    Added small offset to level logger to prevent value from going to zero.

--- src/org/andrewkilpatrick/elmGen/simulator/LevelLogger.java
+++ src/org/andrewkilpatrick/elmGen/simulator/LevelLogger.java
@@ -109,8 +109,9 @@ public class LevelLogger implements AudioSink {
 		// GSW Added some logic to discern between linear and log mode.
 		for(int i = 0; i < len; i += 2) {	
 			if(logMode == 1) {
-				left = Math.abs(Util.regToDouble(dbuf[i]));
-				right = Math.abs(Util.regToDouble(dbuf[i + 1]));
+				// GSW add 0.00001 so we never go to zero (log doesn't like that)
+				left = Math.abs(Util.regToDouble(dbuf[i]) + 0.00001);
+				right = Math.abs(Util.regToDouble(dbuf[i + 1]) + 0.00001);
 				if(left > maxL) {
 					maxL = left;
 				}
